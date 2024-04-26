5e82b62cda75398490533bc27edf0166bbd388fa
UmairSaifi2002 <umairsaifi2002@gmail.com>
2024-04-27 00:43:37 +0530

    updates

---
+++ 01_Basics/hello2_python.py
@@ -0,0 +1,8 @@
+# Here, we import a function from another file see how we import our file and access the function that we need.
+# syntax -> from file_name import function_name
+from hello_python import hello # -> that's how we import our function from another file.
+
+hello("umair of current file")
+# when we import a function from another file
+# and we use that function in a current file
+# then the output is the combination of imported file and current file.
--- 01_Basics/hello_python.py
+++ 01_Basics/hello_python.py
@@ -1,1 +1,9 @@
-print("Hello Python")
+# That's how we print our output
+print("Hello Python") # -> Hello Python
+
+# That's how we create a function in python
+def hello(n):
+    print("Hello",n)
+
+# To call this function we use it's name and pass the parameters
+hello("umair") # -> Hello umair -> here python automatically add space b/w Hello and parameter
