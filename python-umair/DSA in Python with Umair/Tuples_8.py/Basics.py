c9136cd81463564092dbde02f470893fe830248e
UmairSaifi2002 <umairsaifi2002@gmail.com>
2024-12-05 22:48:58 +0530

    Tuple

--- python-umair/DSA in Python with Umair/Tuples_8.py/Basics.py
+++ python-umair/DSA in Python with Umair/Tuples_8.py/Basics.py
@@ -16,3 +16,37 @@ print()
 # Search in a tuple
 newTuple = ('a','b','c','d','e')
 print(newTuple.index('b'))
+# searching using the for loop
+def searchElement(tuple, element):
+    for i in range(len(tuple)):
+        if tuple[i] == element:
+            return f"Element {element} is present at index : {i}"
+    return f"Element {element} is not present in Tuple."
+print(searchElement(newTuple,'f'))
+
+# Tuple Operation / Function
+tup1 = (1,2,3,4)
+tup2 = (5,6,7,8)
+add = tup1 + tup2
+print(add) # (1,2,3,4,5,6,7,8)
+print(tup1*4)
+
+# in operator
+print(4 in tup1)
+print(5 in tup1)
+
+# count() operator it counts the occurence of the number in a tuple
+print(tup1.count(2))
+
+# len() function it return the length of the tuple
+print(len(add))
+
+# max() function
+print(max(tup2))
+
+# we can convert the list to tuple 
+tup = tuple([1,2,3])
+print(type(tup))
+
+
+
