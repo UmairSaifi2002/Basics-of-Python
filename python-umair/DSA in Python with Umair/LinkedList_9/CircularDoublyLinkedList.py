2b944883c6cf19a58ada5f3ccee493939644b953
UmairSaifi <umairsaifi2002@gmail.com>
2024-12-25 11:54:54 +0530

    Circular Linked List

--- python-umair/DSA in Python with Umair/LinkedList_9/CircularDoublyLinkedList.py
+++ python-umair/DSA in Python with Umair/LinkedList_9/CircularDoublyLinkedList.py
@@ -2,11 +2,228 @@ class Node:
     def __init__(self,data):
         self.data = data
         self.next = None
-        self.prev = Node
+        self.prev = None
 
 class CircularDoublyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
-        self.length = 0 
+        self.length = 0
 
+    def __str__(self):
+        temp = self.head
+        result = ''
+        while temp is not None:
+            result += str(temp.data)
+            if temp.next is self.head:
+                break
+            result += ' <-> '
+            temp = temp.next
+        return result
+    
+    def append(self, data):
+        new_node = Node(data)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+            new_node.next = new_node
+            new_node.prev = new_node
+        else:
+            new_node.prev = self.tail
+            new_node.next = self.head
+            self.tail.next = new_node
+            self.head.prev = new_node
+            self.tail = new_node
+        self.length += 1
+    
+    def preppend(self, data):
+        new_node = Node(data)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+            new_node.next = new_node
+            new_node.prev = new_node
+        else:
+            new_node.next = self.head
+            new_node.prev = self.tail
+            self.head.prev = new_node
+            self.tail.next = new_node
+            self.head = new_node
+        self.length += 1
+    
+    def insert(self, index, data):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            self.preppend(data)
+        elif index == self.length - 1:
+            self.append(data)
+        else:
+            new_node = Node(data)
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next
+            new_node.next = temp.next
+            new_node.prev = temp
+            temp.next.prev = new_node
+            temp.next = new_node
+        self.length += 1
+    
+    def traverse(self): 
+        temp = self.head
+        while temp is not None:
+            print(temp.data,end=' ')
+            if temp.next is self.head:
+                break
+            temp = temp.next
+    
+    def ReverseTraverse(self):
+        temp = self.tail
+        while temp is not None:
+            print(temp.data,end=' ')
+            if temp.prev is self.tail:
+                break
+            temp = temp.prev
+    
+    def search(self,index):
+        temp = self.head
+        while temp is not None:
+            if temp.data == index:
+                return True
+            temp = temp.next
+            if temp is self.head: # to escape from the loop
+                break
+        return False
+    
+    def Get(self, index):
+        # Check if the index is out of bounds or the list is empty
+        if index < 0 or index >= self.length or self.head is None:
+            return None
+        # Optimize traversal based on index position
+        if index <= self.length // 2:
+            # Traverse from the head
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next
+        else:
+            # Traverse from the tail
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        return temp
+    
+    def set_value(self, index, value):
+        if index < 0 or index >= self.length or self.head is None:
+            return None
+        if index == 0:
+            self.head.data = value
+        elif index == self.length - 1:
+            self.tail.data = value
+        else:
+            self.Get(index).data = value
+    
+    def pop_first(self):
+        if self.head is None:
+            return None  # Empty list, nothing to pop
+        popped = self.head  # Save the current head node
+        if self.length == 1:
+            # Case 1: Single-node list
+            self.head = None
+            self.tail = None
+        else:
+            # Case 2: Multiple-node list
+            self.head = self.head.next  # Update head to the next node
+            self.head.prev = None       # Remove reference to the old head
+            popped.next = None          # Clean up popped node's next pointer
+            self.tail.next = self.head  # Update the tail's next pointer
+            self.head.prev = self.tail  # Update the head's prev pointer
+        self.length -= 1  # Decrement the length of the list
+        return popped
+    
+    def pop(self):
+        if self.head is None:
+            return None
+        popped = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = self.head
+            self.head.prev = self.tail
+        popped.next = None
+        popped.prev = None
+        self.length -= 1
+        return popped
+    
+    def remove(self, index):
+        temp = self.head
+        if index < 0 or index >= self.length or self.length == 0:
+            return None
+        if index == 0:
+            self.pop_first()
+        elif index == self.length - 1:
+            self.pop()
+        else:
+            for _ in range(index):
+                temp = temp.next
+            temp.prev.next = temp.next
+            temp.next.prev = temp.prev
+            temp.next = None
+            temp.prev = None
+            self.length -= 1
+        return temp
+
+cdll = CircularDoublyLinkedList()
+# append the elements
+cdll.append(10)
+cdll.append(20)
+cdll.append(30)
+cdll.append(40)
+cdll.append(50)
+cdll.append(60)
+print(cdll) # 10 <-> 20 <-> 30 <-> 40 <-> 50 <-> 60
+
+# inserting the element
+cdll.insert(2, 350)
+print(cdll)
+
+# traversing the elements
+cdll.traverse() # 10 20 30 350 40 50 60
+print()
+
+# rversing the elements
+cdll.ReverseTraverse() # 60 50 40 350 30 20 10
+print()
+
+# searching the elements
+print(cdll.search(20)) # True
+print(cdll.search(40)) # True
+print(cdll.search(70)) # False
+
+# getting the value of the elements
+print(cdll.Get(5).data) # 60
+
+# setting value of the elements
+cdll.set_value(0, 100) 
+cdll.set_value(2, 300)
+cdll.set_value(5, 600)
+print(cdll) # 100 <-> 20 <-> 300 <-> 350 <-> 40 <-> 50 <-> 600
+
+# inserting the elements 
+cdll.insert(0,0)
+cdll.insert(6,7)
+print(cdll) # 0 <-> 100 <-> 20 <-> 300 <-> 350 <-> 40 <-> 600 <-> 7 <-> 60
+
+# poping the elements
+print(cdll.pop_first().data) # 0
+print(cdll.pop().data) # 60
+print(cdll) # 100 <-> 20 <-> 300 <-> 350 <-> 40 <-> 600 <-> 7
+
+# removing the element
+# print(cdll.remove(2).data) # 300
+# print(cdll) # 100 <-> 20 <-> 350 <-> 40 <-> 600 <-> 7
+# print(cdll.remove(0).data) # 100
+# print(cdll) # 20 <-> 350 <-> 40 <-> 600 <-> 7
+# print(cdll.remove(4).data) # 7
+# print(cdll) # 20 <-> 350 <-> 40 <-> 600
--- python-umair/DSA in Python with Umair/LinkedList_9/Doubly_LinkedList.py
+++ python-umair/DSA in Python with Umair/LinkedList_9/Doubly_LinkedList.py
@@ -1,198 +1,184 @@
 class Node:
-    def __init__(self,data):
-        self.data = data
-        self.next = None
-        self.prev = None
+    def __init__(self, data):
+        self.data = data  # Store the value of the node
+        self.next = None  # Pointer to the next node
+        self.prev = None  # Pointer to the previous node
 
 class DoublyLinkedList:
     def __init__(self):
-        self.head = None
-        self.tail = None
-        self.length = 0
-    
+        self.head = None  # First node in the list
+        self.tail = None  # Last node in the list
+        self.length = 0   # Track the number of nodes in the list
+
     def __str__(self):
+        # Return the string representation of the list
         temp = self.head
         result = 'None <- '
-        while temp is not None:
+        while temp:
             result += str(temp.data)
-            if temp.next is not None:
+            if temp.next:
                 result += ' <-> '
             temp = temp.next
         result += ' -> None'
         return result
-    
-    def append(self,data):
-        temp = Node(data)
-        if self.head is None:
-            self.head = temp
-            self.tail = temp
-        else:
-            self.tail.next = temp
-            temp.prev = self.tail
-            self.tail = temp
+
+    def append(self, data):
+        # Add a node to the end of the list
+        new_node = Node(data)
+        if not self.head:  # If the list is empty
+            self.head = self.tail = new_node
+        else:  # Otherwise, attach to the end
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
         self.length += 1
-    
-    def preppend(self,data):
-        temp = Node(data)
-        if self.head is None:
-            self.head = temp
-            self.tail = temp
-        else:
-            temp.next = self.head
-            self.head.prev = temp
-            self.head = temp
+
+    def preppend(self, data):
+        # Add a node to the beginning of the list
+        new_node = Node(data)
+        if not self.head:  # If the list is empty
+            self.head = self.tail = new_node
+        else:  # Otherwise, attach to the front
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
         self.length += 1
-    
+
     def traverse(self):
+        # Traverse and print the list from head to tail
         temp = self.head
-        while temp is not None:
-            print(temp.data,end=' ')
+        while temp:
+            print(temp.data, end=' ')
             temp = temp.next
-    
+        print()
+
     def ReverseTreverse(self):
+        # Traverse and print the list from tail to head
         temp = self.tail
-        while temp is not None:
-            print(temp.data,end=' ')
+        while temp:
+            print(temp.data, end=' ')
             temp = temp.prev
-    
-    def search(self,target):
+        print()
+
+    def search(self, target):
+        # Search for a value in the list
         temp = self.head
-        while temp is not None:
-            if target == temp.data:
+        while temp:
+            if temp.data == target:
                 return True
             temp = temp.next
         return False
-    
+
     def Get(self, index):
-        # Validate the index
+        # Retrieve the node at a specific index
         if index < 0 or index >= self.length:
             return None
-        # Optimized traversal
-        if index < self.length // 2:
-            # Traverse from the head
+        if index < self.length // 2:  # Traverse from head
             curr = self.head
             for _ in range(index):
                 curr = curr.next
-        else:
-            # Traverse from the tail
+        else:  # Traverse from tail
             curr = self.tail
             for _ in range(self.length - 1, index, -1):
                 curr = curr.prev
         return curr
-    
-    def SetValue(self,index,data):
-        node = self.Get(index)
-        if node:
-            node.data = data
-            return True
-        return False
-    
+
     def insert(self, index, data):
+        # Insert a node at a specific index
         if index < 0 or index > self.length:
             print('Index Out of Bounds')
             return
-        new_node = Node(data)
         if index == 0:
             self.preppend(data)
-            return 
-        if index == self.length:
+        elif index == self.length:
             self.append(data)
-            return
-        temp_node = self.Get(index-1)
-        new_node.next = temp_node.next
-        new_node.prev = temp_node
-        temp_node.next = new_node
-        temp_node.next.prev = new_node
-    
+        else:
+            new_node = Node(data)
+            prev_node = self.Get(index - 1)
+            next_node = prev_node.next
+            new_node.prev = prev_node
+            new_node.next = next_node
+            prev_node.next = new_node
+            next_node.prev = new_node
+            self.length += 1
+
     def popFirst(self):
-        if self.head is None:
+        # Remove and return the first node
+        if not self.head:  # Empty list
             return None
         popped_node = self.head
-        if self.length == 1:
-            self.head = None
-            self.tail = None
+        if self.length == 1:  # Single-node list
+            self.head = self.tail = None
         else:
             self.head = popped_node.next
             self.head.prev = None
-            popped_node.next = None
+        popped_node.next = None
         self.length -= 1
         return popped_node
-    
+
     def pop(self):
-        # Case 1: Empty list
-        if self.head is None:
+        # Remove and return the last node
+        if not self.head:  # Empty list
             return None
-        # Save the tail node to be removed
         popped_node = self.tail
-        # Case 2: Single-node list
-        if self.head == self.tail:
-            self.head = None
-            self.tail = None
-        # Case 3: More than one node
+        if self.length == 1:  # Single-node list
+            self.head = self.tail = None
         else:
-            self.tail = popped_node.prev  # Update tail to the previous node
-            self.tail.next = None         # Disconnect the last node
-            popped_node.prev = None       # Clean up the popped node's references
-        # Update the length of the list
+            self.tail = popped_node.prev
+            self.tail.next = None
+        popped_node.prev = None
         self.length -= 1
-        # Return the removed node
         return popped_node
 
     def remove(self, index):
-        # Case 1: Empty list
-        if self.head is None:
+        # Remove a node at a specific index
+        if not self.head:  # Empty list
             return None
-        # Case 2: Index out of bounds
         if index < 0 or index >= self.length:
-            raise IndexError("Index out of bounds")
-        # Case 3: Remove the first node
+            print('Index Out of Bounds')
+            return
         if index == 0:
             return self.popFirst()
-        # Case 4: Remove the last node
         if index == self.length - 1:
             return self.pop()
-        # Case 5: Remove an intermediate node
-        popped_node = self.Get(index)  # Get the node at the specified index
-        previous_node = popped_node.prev
-        next_node = popped_node.next
-        # Update pointers to remove the node
-        previous_node.next = next_node
-        next_node.prev = previous_node
-        # Clean up the removed node's references
-        popped_node.prev = None
-        popped_node.next = None
-        # Update the length
+        node_to_remove = self.Get(index)
+        prev_node = node_to_remove.prev
+        next_node = node_to_remove.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+        node_to_remove.next = node_to_remove.prev = None
         self.length -= 1
-        # Return the removed node
-        return popped_node
+        return node_to_remove
 
 
+# Testing the DoublyLinkedList class
 dll = DoublyLinkedList()
 dll.append(1)
 dll.append(2)
 dll.append(3)
 dll.append(4)
 dll.append(5)
-print(dll) # None <- 1 <-> 2 <-> 3 <-> 4 <-> 5 -> None
+print(dll)  # None <- 1 <-> 2 <-> 3 <-> 4 <-> 5 -> None
 
 dll.preppend(0)
 dll.preppend(-1)
-print(dll) # None <- -1 <-> 0 <-> 1 <-> 2 <-> 3 <-> 4 <-> 5 -> None
+print(dll)  # None <- -1 <-> 0 <-> 1 <-> 2 <-> 3 <-> 4 <-> 5 -> None
 
-dll.traverse()
-print()
-dll.ReverseTreverse()
-print()
-print(dll.search(4))
-print(dll.search(7))
+dll.traverse()  # -1 0 1 2 3 4 5
+dll.ReverseTreverse()  # 5 4 3 2 1 0 -1
+print(dll.search(4))  # True
+print(dll.search(7))  # False
 
-print(dll.Get(3).data)
-print(dll.Get(2).data)
-print(dll.Get(-1))
-print(dll.Get(7))
+dll.insert(3, 100)
+print(dll)  # None <- -1 <-> 0 <-> 1 <-> 100 <-> 2 <-> 3 <-> 4 <-> 5 -> None
 
-dll.insert(3,100)
-print(dll)
+dll.popFirst()
+print(dll)  # None <- 0 <-> 1 <-> 100 <-> 2 <-> 3 <-> 4 <-> 5 -> None
 
-print(dll.popFirst().data)
+dll.pop()
+print(dll)  # None <- 0 <-> 1 <-> 100 <-> 2 <-> 3 <-> 4 -> None
+
+dll.remove(3)
+print(dll)  # None <- 0 <-> 1 <-> 100 <-> 2 <-> 4 -> None
+dll.remove(2)
 print(dll)
