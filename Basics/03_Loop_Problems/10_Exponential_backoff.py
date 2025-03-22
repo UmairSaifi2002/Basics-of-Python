ca0a73a2e82f3c911f73f55bda2d4ba436ff620e
Umair Saifi <umairsaifi2002@gmail.com>
2025-03-22 05:20:56 +0000

    some new changes

--- Basics/03_Loop_Problems/10_Exponential_backoff.py
+++ Basics/03_Loop_Problems/10_Exponential_backoff.py
@@ -9,7 +9,7 @@ max_retries = 5
 attempts = 0
 
 while attempts < max_retries:
-    print("Attempts", attempts+1 ,"- wait time", wait_time)
+    print("Attempts", attempts+1 ,"- wait time", wait_time,"sec")
     time.sleep(wait_time)
     wait_time *= 2
     attempts += 1
--- Basics/03_Loop_Problems/5_Find_the_First_Non-Repeated_Character.py
+++ Basics/03_Loop_Problems/5_Find_the_First_Non-Repeated_Character.py
@@ -6,25 +6,25 @@ user_input = input("Enter your String : ")
 non_repeated_character = ""
 
 # by me
-for char1 in user_input:
-    count = 0
-    for char2 in user_input:
-        if char1 == char2:
-            count = count + 1
-    if count == 1:
-        non_repeated_character = char1
-        break
+# for char1 in user_input:
+#     count = 0
+#     for char2 in user_input:
+#         if char1 == char2:
+#             count = count + 1
+#     if count == 1:
+#         non_repeated_character = char1
+#         break
 
-if non_repeated_character:
-    print("The first non-repeated character is:", non_repeated_character)
-else:
-    print("No non-repeated character found.")
+# if non_repeated_character:
+#     print("The first non-repeated character is:", non_repeated_character)
+# else:
+#     print("No non-repeated character found.")
 
 
 # by hitesh sir
-# for char in user_input:
-#     if user_input.count(char) == 1:
-#         non_repeated_character = char
-#         break
+for char in user_input:
+    if user_input.count(char) == 1:
+        non_repeated_character = char
+        break
 
-# print("The first non-repeated character in '{}' is -> '{}'".format(user_input, non_repeated_character))
+print("The first non-repeated character in '{}' is -> '{}'".format(user_input, non_repeated_character))
