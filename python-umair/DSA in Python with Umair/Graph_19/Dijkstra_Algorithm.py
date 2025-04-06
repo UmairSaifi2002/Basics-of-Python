842c5037e1d86490031b49265d79d16515de7281
UmairSaifi <umairsaifi2002@gmail.com>
2025-04-06 21:42:07 +0530

    New Commit

--- python-umair/DSA in Python with Umair/Graph_19/Dijkstra_Algorithm.py
+++ python-umair/DSA in Python with Umair/Graph_19/Dijkstra_Algorithm.py
@@ -1,5 +1,5 @@
 # Dijkstra's Algorithm
-
+import heapq
 class Edge:
     def __init__(self, weight, start_node, target_node):
         self.weight = weight
@@ -10,10 +10,12 @@ class Node:
     def __init__(self, name):
         self.name = name
         self.visited = False
+        # Previous node that we come to this node
         self.predecessor = None
         self.neighbors = []
         self.min_distance = float('inf')
 
+    # this a function is to calculate less than function
     def __lt__(self, other_node):
         return self.min_distance < other_node.min_distance
     
@@ -21,9 +23,74 @@ class Node:
         edge = Edge(weight, self, destination_vertex)
         self.neighbors.append(edge)
 
-def Dijkstra_Algorithm():
-    pass
+class Dijkstra_Algorithm:
+    def __init__(self):
+        self.heap = []
 
-a = Node('A')
-b = Node('B')
-print(a < b)
+    def calculate(self,start_vertex):
+        start_vertex.min_distance = 0
+        heapq.heappush(self.heap, start_vertex)
+        while self.heap:
+            # pop elment with its lowest distance
+            actual_vertex = heapq.heappop(self.heap)
+            if actual_vertex.visited:
+                continue
+            # consider the neighbors
+            for edge in actual_vertex.neighbors:
+                start = edge.start_node
+                target = edge.target_node
+                new_distance = start.min_distance + edge.weight
+                if new_distance < target.min_distance :
+                    target.min_distance = new_distance
+                    target.predecessor = start
+                    # Update the Heap
+                    heapq.heappush(self.heap, target)
+            actual_vertex.visited = True
+    
+    def get_shortest_path(self, vertex):
+        print(f'The shortest path to the vertex is : {vertex.min_distance}')
+        actual_vertex = vertex
+        while actual_vertex is not None:
+            print(actual_vertex.name, end=' ')
+            actual_vertex = actual_vertex.predecessor
+
+
+# a = Node('A')
+# b = Node('B')
+# print(a > b)
+
+# Step 1 - Create Nodes
+nodeA = Node('A')
+nodeB = Node('B')
+nodeC = Node('C')
+nodeD = Node('D')
+nodeE = Node('E')
+nodeF = Node('F')
+nodeG = Node('G')
+nodeH = Node('H')
+
+nodeA.add_edge(6, nodeB)
+nodeA.add_edge(10, nodeC)
+nodeA.add_edge(9, nodeD)
+
+nodeB.add_edge(16, nodeE)
+nodeB.add_edge(13, nodeF)
+nodeB.add_edge(5, nodeD)
+
+nodeC.add_edge(6, nodeD)
+nodeC.add_edge(5, nodeH)
+nodeC.add_edge(21, nodeG)
+
+nodeD.add_edge(8, nodeF)
+nodeD.add_edge(7, nodeH)
+
+nodeE.add_edge(10, nodeG)
+
+nodeF.add_edge(4, nodeE)
+nodeF.add_edge(12, nodeG)
+
+nodeH.add_edge(14, nodeG)
+
+algorithm = Dijkstra_Algorithm()
+algorithm.calculate(nodeA)
+algorithm.get_shortest_path(nodeG)
